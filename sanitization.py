84333b7b8d3085b5436b7730985aed53d7fcbdf1
Luan Menezes <luancmenezes@gmail.com>
2019-08-06 18:53:13 -0300

    correção na classe

--- sanitization.py
+++ sanitization.py
@@ -5,19 +5,19 @@ class Sanitization:
         self.types = list()
         self.path = path
         self.tokens = list()
-    def extractTokens(tokens):
+    def extractTokens(self,tokens):
         return tokens[0].value,tokens[1],tokens[2].value
-    def ctr2def(token):
+    def ctr2def(self,token):
         return token.value[1:-1]
-    def parseTokens(self)
-        file_ = open(self.path, "r")
-        raw = file_.read()
+    def parseTokens(self):
+#         file_ = open(self.path, "r")
+        raw = path
         parsed = sqlparse.parse(raw)[0]
-        self.tokens = [sql for sql in parsed.tokens if sql.is_group]  
-        return extractTokens(self.tokens)
+        self.tokens = [sql for sql in parsed.tokens if sql.is_group]
+        return self.extractTokens(self.tokens)
     
-    def parse_identifier_list(self,token):
-    #     if is_identifier_list(token):
+#     def parse_identifier_list(self,token):
+#     #     if is_identifier_list(token):
 
 
     #sqlparse.keywords.KEYWORDS.get('REAL')
@@ -31,21 +31,22 @@ class tokensUtils:
     def is_identifier(token):
         return isinstance(token, sqlparse.sql.Identifier) 
 
-def main()
+def main():
     s = Sanitization('create.ctr')
-    _tb,_def,_db = Sanitization.parseTokens()
-    ## write def = Sanitization.ctr2def(_def)
-    for token in _def.tokens:
-        if token.is_group:
-            if tokensUtils.is_identifier(token):
-                x.names.append(token.value)
-            elif tokensUtils.is_function(token):
-                x.types.append(token.value)
-            else:
-                Sanitization.parse_identifier_list(token.value)
+    _tb,_def,_db = s.parseTokens()
+    #print(s.ctr2def(_def))
+    ## write def = 
+#     for token in _def.tokens:
+#         if token.is_group:
+#             if tokensUtils.is_identifier(token):
+#                 x.names.append(token.value)
+#             elif tokensUtils.is_function(token):
+#                 x.types.append(token.value)
+#             else:
+#                 Sanitization.parse_identifier_list(token.value)
     
-        # if sqlparse.keywords.KEYWORDS.get(x.get_name()) != None
-        #     value = x.get_name()
+#         # if sqlparse.keywords.KEYWORDS.get(x.get_name()) != None
+#         #     value = x.get_name()
 if __name__ == "__main__":
     main()  
 			
